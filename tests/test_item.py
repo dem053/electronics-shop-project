4832970c3d341c42a178fb26abd33694aee2a6e6
Acer <dmv053@gmail.com>
2023-10-09 23:00:45 +0400

    доработаны тесты item_instantiate_from_csv

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,34 +1,36 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
+import pytest, os
 
 from src.item import Item
 from src.phone import Phone
+from src.instantiate_csv_error import InstantiateCSVError
+
 
 @pytest.fixture
 def item_test():
     return Item("Миксер", 10.2, 15)
 
+
 @pytest.fixture
 def phone_test():
     return Phone("Хонор", 100.1, 5, 1)
 
-def test_item_init(item_test):
 
+def test_item_init(item_test):
     assert item_test.name == "Миксер"
     assert item_test.price == 10.2
     assert item_test.quantity == 15
 
 
-def test_item_calculate_total_price (item_test):
-
+def test_item_calculate_total_price(item_test):
     assert item_test.calculate_total_price() == 153
 
 
-def test_item_apply_discount (item_test):
-
+def test_item_apply_discount(item_test):
     Item.pay_rate = 0.5
     item_test.apply_discount()
     assert item_test.price == 5.1
+
 
 def test_item_instantiate_from_csv():
     Item.all = []
@@ -39,14 +41,18 @@ def test_item_instantiate_from_csv():
     assert Item.all[2].quantity == 5
     assert Item.all[3].price == 50.5
 
+
 def test_item__repr__(item_test):
     assert repr(item_test) == "Item('Миксер', 10.2, 15)"
+
 
 def test_item__str__(item_test):
     assert str(item_test) == "Миксер"
 
-def test_item_add (item_test, phone_test):
+
+def test_item_add(item_test, phone_test):
     assert item_test + phone_test == 20
+
 
 def test_item_add_noclass(phone_test):
     '''
@@ -57,4 +63,18 @@ def test_item_add_noclass(phone_test):
     except Exception as e:
         assert e
 
+
+def test_item_instantiate_from_csv_filenotfounderror():
+    file_name = 'tests/nottest.csv'
+    n_dir, n_file = file_name.split('/')
+    file_name_t = os.path.join('..', n_dir, n_file)
+    with pytest.raises(FileNotFoundError, match=f'Файл {file_name} не найден'):
+        Item.instantiate_from_csv(file_name)
+
+def test_item_instantiate_from_csv_file_damaged():
+    file_name = 'tests/test_items_damaged.csv'
+    n_dir, n_file = file_name.split('/')
+    file_name_t = os.path.join('..', n_dir, n_file)
+    with pytest.raises(InstantiateCSVError, match=f'Файл {str(file_name)} поврежден'):
+        Item.instantiate_from_csv(file_name)
 
